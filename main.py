e5b456afec6a3f1e0ffaff86a70471d46a1240f1
unknown <90915@evdi.airport.kr>
2026-06-05 13:44:17 +0900

    feat: Add Korean article crawling and morpheme analysis features

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def run_analysis(analyzer):
 def main():
     total_start = time.time()
     
-    #run_crawling(NewsCrawler(api_key=CONFIG['API_KEY'], save_dir=CONFIG['DATA_FOLDER']), CONFIG['TOPICS'])
+    run_crawling(NewsCrawler(api_key=CONFIG['API_KEY'], save_dir=CONFIG['DATA_FOLDER']), CONFIG['TOPICS'])
     run_analysis(TextAnalyzer(data_dir=CONFIG['DATA_FOLDER']))
     
     print(f"\n>>> 전체 작업 완료 (총 {time.time() - total_start:.2f}초)")
